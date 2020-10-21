6ee61c1e6c28e5f5e067b97fc6b663b78c06bb53
Michal <info@gateweb.cz>
2020-10-21 09:56:39 +0200

    oprava zápisu

---
+++ htmlelements.php
@@ -0,0 +1,25 @@
+<?php
+// hlavička HTML stránky
+// INDEX - ALL
+function htmlHeaderAll()
+{
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ISTUDRZBA</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Bootstrap-Calendar.css">
+    <link rel="stylesheet" href="assets/css/Card-Deck.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<?php
+}
+// KONEC HLAVIČKA HTML STRÁNKY
+?>
